ed33e69fa35214c87686d33d1c49e3a5b51245ab
Sagar Gajare <56780589+Sagargajare@users.noreply.github.com>
2021-08-24 16:23:47 +0530

    resume link updated

--- src/Components/About.js
+++ src/Components/About.js
@@ -80,7 +80,7 @@ export default function About() {
       <b />
       <div className={classes.ctcButtons}>
         <Button
-          href="https://drive.google.com/file/d/1QbOrh6FwyvRCEho5HptRzz7VOkkSPPnd/view?ths=true"
+          href="https://github.com/Sagargajare/myportfolio/raw/main/src/Resume/Sagar's%20Resume-Software%20Developer.pdf"
           variant="contained"
           color="primary"
         >
